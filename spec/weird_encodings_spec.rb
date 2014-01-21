3f8ba124e3339249571dc2ec4c338b2503ec5501
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-01-21 01:03:46 +0100

    mark one test with missing data as pending
    The best I can do today.

--- spec/weird_encodings_spec.rb
+++ spec/weird_encodings_spec.rb
@@ -1,15 +1,18 @@
 require "#{File.dirname(__FILE__)}/spec_helper"
 
 describe "weird encodings" do
+  # FIXME: The whole point of this was to show that real life data could be parsed.
+  # I should make a decision to either remove the test or find a way to make vcards with different sorts of charsets.
+
   before do
+    pending "The vcf-files used in these tests have been censored."
     @files = Dir.glob("#{VCARDS_ROOT}/encodings/*.vcf")
     @files.should_not be_empty
   end
 
   specify "should be fixable" do
+    pending "The vcf-files used in these tests have been censored."
     @files.each do |file|
-      # puts file
-      # Vcard.fix_and_clean(File.read(file))
       lambda { Vcard.fix_and_clean(File.read(file)) }.should_not raise_error
     end
   end
